bae6dc46030add289ebabfa2acbd8d6db692ce65
Stephen-254 <stephenmutheu@gmail.com>
2024-09-26 10:04:33 +0300

    Error ywo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const dateRegularExp = / ^(?:(\d{2})[-\/](\d{2})[-\/](\d{2})|(\d{4})[-\/](\d{2})[-\/](\d{2}))$/;
-const dateRegularExpTwo = /^[1-7]$/
+const generalDateRegularExp = / ^(?:(\d{2})[-\/](\d{2})[-\/](\d{2})|(\d{4})[-\/](\d{2})[-\/](\d{2}))$/;
+const dateRegularExp = /^[1-31]$/;
+const monthRegularExp = /^(0[1-9]|1[0-2])$/;
 
 const secondError = document.getElementsByClassName('error-two');
 const thirdError = document.getElementsByClassName('error-three');
@@ -9,15 +10,22 @@ const dayValue = document.getElementsByClassName('day')[0];
 const monthValue = document.getElementsByClassName('month')[0];
 const yearValue = document.getElementsByClassName('year')[0];
 
+const dayText = document.getElementById('day-text');
+const monthText = document.getElementById('month-text');
+const yearText = document.getElementById('year-text');
+
 let isInputValid = true; 
 
 
-function showErrorOne() {
+
+
+/*function showErrorOne() {
     
-    /* First error for the inputs */
+    First error for the inputs 
     if(dayValue.value === "") {
         firstError[0].style.display = "inline";
         dayValue.style.borderColor = 'red';
+        dayText.style.color = 'red'
         isInputValid = false;
     } 
     else{
@@ -25,11 +33,10 @@ function showErrorOne() {
         secondError[0].style.display = "none";
     }
 
-
-
     if(monthValue.value === "") {
         firstError[1].style.display = "inline";
         monthValue.style.borderColor = 'red';
+        monthText.style.color = 'red'
         isInputValid = false;
     } else{
         monthValue.style.borderColor = 'black';
@@ -39,29 +46,37 @@ function showErrorOne() {
     if(yearValue.value === "") {
         firstError[2].style.display = "inline";
         yearValue.style.borderColor = 'red';
+        yearText.style.color = 'red'
         isInputValid = false;
     } else{
         yearValue.style.borderColor = 'black';
         firstError[2].style.display = "none";
     }
-}
+} */
 
 
-/*function showErrorTwo() {
-    if (!dateRegularExpTwo.test(dayValue.value)) {
+function showErrorTwo() {
+    /*if (!dateRegularExp.test(dayValue.value)) {
+        secondError[0].style.display = "inline";
+        isInputValid = false;
+      } else {
+        secondError[0].style.display = "none";
+      }*/
+
+      if (!monthRegularExp.test(monthValue.value)) {
         secondError[0].style.display = "inline";
         isInputValid = false;
       } else {
         secondError[0].style.display = "none";
       }
-}*/
+}
 
-/*function validateForm() {
-    showErrorOne();
+function validateForm() {
+    /*showErrorOne();*/
     showErrorTwo()
-}*/
+}
 
-document.getElementById("button").addEventListener("click", showErrorOne);
+document.getElementById("button").addEventListener("click", showErrorTwo);
 
 
 
